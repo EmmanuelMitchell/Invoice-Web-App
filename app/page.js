d98441ac9dabf4f314c14a6686cb530b333f7f7b
EmmanuelMitchell <emmanuelmitchell1998@gmail.com>
2024-03-04 15:37:57 +0000

    resolving some issuse

--- app/page.js
+++ app/page.js
@@ -39,12 +39,10 @@ export default function Home() {
           Welcom to Invoice App V1
         </h2>
         <p className="text-gray-500">
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book. It has survived not only five centuries,
-          but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of
-          Letraset sheets containing Lorem
+          Easily create invoices for yourself, your clients all at the
+          convenience of your mobile phone or PC. Version 2 offers improved
+          performance, better responsiveness on mobile, and better UI design by
+          Thomas Sankara.
         </p>
 
         {userId ? (
